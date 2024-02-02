868e90ab42048ec823b912f71f641c69ee9156b9
Bikathi Martin <bikathimartin@outlook.com>
2024-02-02 09:22:01 +0300

    Create folder structures for layouts and pages

---
+++ layouts/default-layout.vue
@@ -0,0 +1,15 @@
+<template>
+	<template>
+		<nav class="p-2"></nav>
+		<main class="p-2 w-full md:w-3/4 max-3/4">
+			<div>
+				<slot />
+			</div>
+			<aside>
+				<!-- section for random common articles -->
+			</aside>
+		</main>
+	</template>
+</template>
+
+<script setup lang="ts"></script>
