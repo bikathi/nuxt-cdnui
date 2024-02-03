17789bf6a82ef6786682203f7171c5af77466e79
Bikathi Martin <bikathimartin@outlook.com>
2024-02-03 22:13:41 +0300

    Create subfolders for blog and core app components

---
+++ components/blog/BlogHighlight.vue
@@ -0,0 +1,12 @@
+<template>
+	<div
+		class="p-3 rounded-lg max-w-full my-3 transition-all hover:shadow-md hover:scale-101 hover duration-300">
+		<h1 class="text-xl font-bold uppercase text-blue-500">Hello world</h1>
+		<h2 class="font-semibold antialiased">
+			This is an example topic to show how the article titles will look
+			like.
+		</h2>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ components/core/Logo.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="flex items-center space-x-1">
+		<h1>Logo</h1>
+		<img
+			src=""
+			alt="App Logo" />
+	</div>
+</template>
+
+<script setup lang="ts"></script>
