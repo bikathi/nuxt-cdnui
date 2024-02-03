6d0623c14d7ca28c3ad161af4548844e58f4bb16
Bikathi Martin <bikathimartin@outlook.com>
2024-02-03 22:25:37 +0300

    Create component for displaying app logo

--- components/core/Logo.vue
+++ components/core/Logo.vue
@@ -1,10 +1,18 @@
 <template>
 	<div class="flex items-center space-x-1">
-		<h1>Logo</h1>
+		<h1 class="font-semibold text-3xl">Logo</h1>
 		<img
-			src=""
+			v-if="props.showLogo"
+			:src="props.logoLink"
 			alt="App Logo" />
 	</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	export interface Props {
+		showLogo?: boolean;
+		logoLink?: string;
+	}
+
+	const props = defineProps<Props>();
+</script>
