ce9096efad73c79a297d814185eaa452865ae6f4
Bikathi Martin <bikathimartin@outlook.com>
2024-02-03 23:07:47 +0300

    Mount <ThemeSwitcher /> and <SearchButton /> components

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,6 +1,12 @@
 <template>
-	<nav class="p-2 px-6 h-20 flex items-center space-between shadow-md">
-		<Logo :show-logo="false" />
+	<nav class="p-2 px-6 h-20 flex space-between shadow-md">
+		<div class="w-3/4 h-full flex items-center">
+			<Logo :show-logo="false" />
+		</div>
+		<div class="w-1/4 flex justify-end space-x-2 items-center h-full">
+			<ThemeSwitcher />
+			<SearchButton />
+		</div>
 	</nav>
 </template>
 
