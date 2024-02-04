8a417dbcbe4468a230302541cfc3abdcbd8d99ea
Bikathi Martin <bikathimartin@outlook.com>
2024-02-04 21:05:46 +0300

    Create component to show small blog details

---
+++ components/blog/MiniBlogStory.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="p-2 h-fit">
+		<div class="w-full h-[17rem]">
+			<img
+				src="/images/sample-image.jpg"
+				alt="Main Article Image"
+				class="max-h-full object-cover w-full rounded-lg" />
+		</div>
+		<h1 class="text-2xl font-bold mt-2">
+			Top Things To Plan for As You Save Up For 2024 Festive Holidays
+		</h1>
+	</div>
+</template>
+
+<script setup lng="ts"></script>
