d4f8523384c5d9bc01aac8824c76351ebde8d5e4
Bikathi Martin <bikathimartin@outlook.com>
2024-02-04 21:06:22 +0300

    Create layout for encompassing blog editor and reader

---
+++ layouts/blog-layout.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="min-h-screen flex flex-col dark:bg-darkest">
+		<Navbar />
+		<main class="px-36 w-full flex-grow flex">
+			<div class="w-[75%]">
+				<slot />
+			</div>
+		</main>
+	</div>
+</template>
