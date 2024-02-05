7036c2c83e16edd1232f5e68f1b6435da888a4da
Bikathi Martin <bikathimartin@outlook.com>
2024-02-05 22:12:55 +0300

    Make grids responsive

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
 	<main class="h-full">
 		<MainStory />
 		<h1 class="text-4xl font-bold ml-1 my-2">You Also May Like</h1>
-		<div class="grid grid-cols-2">
+		<div class="grid grid-cols-1 md:grid-cols-2">
 			<MiniBlogStory
 				v-for="(a, index) in 6"
 				:key="index" />
