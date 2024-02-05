1075dc21046aa5dc935c73dd4cc84d6c2214c03e
Bikathi Martin <bikathimartin@outlook.com>
2024-02-05 22:12:24 +0300

    Trial at making UI responsive

--- layouts/blog-layout.vue
+++ layouts/blog-layout.vue
@@ -1,5 +1,5 @@
 <template>
-	<main class="min-h-screen dark:bg-darkest">
+	<main class="min-h-screen flex flex-col dark:bg-darkest">
 		<Navbar />
 		<slot />
 	</main>
--- layouts/default-layout.vue
+++ layouts/default-layout.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="min-h-screen flex flex-col dark:bg-darkest">
 		<Navbar />
-		<main class="px-64 w-full flex-grow flex">
-			<div class="w-[70%]">
+		<main class="px-2 lg:px-64 w-full flex-grow flex">
+			<div class="w-full lg:w-[70%]">
 				<slot />
 			</div>
-			<aside class="min-w-[15%] pt-1 px-2 fixed right-14">
+			<aside class="min-w-[15%] pt-1 px-2 fixed right-14 hidden lg:flex">
 				<!-- section for random common articles -->
 				<h1 class="text-4xl font-bold mb-4 ml-3">Top Picks</h1>
 				<BlogHighlight
