76d937825b1baa7279c46dc0af3983a0800610a9
Bikathi Martin <bikathimartin@outlook.com>
2024-02-05 19:35:19 +0300

    Update UI of existing layouts

--- layouts/blog-layout.vue
+++ layouts/blog-layout.vue
@@ -1,10 +1,6 @@
 <template>
-	<div class="min-h-screen flex flex-col dark:bg-darkest">
+	<main class="min-h-screen dark:bg-darkest">
 		<Navbar />
-		<main class="px-36 w-full flex-grow flex">
-			<div class="w-[75%]">
-				<slot />
-			</div>
-		</main>
-	</div>
+		<slot />
+	</main>
 </template>
--- layouts/default-layout.vue
+++ layouts/default-layout.vue
@@ -1,15 +1,19 @@
 <template>
-	<template>
-		<nav class="p-2"></nav>
-		<main class="p-2 w-full md:w-3/4 max-3/4">
-			<div>
+	<div class="min-h-screen flex flex-col dark:bg-darkest">
+		<Navbar />
+		<main class="px-64 w-full flex-grow flex">
+			<div class="w-[70%]">
 				<slot />
 			</div>
-			<aside>
+			<aside class="min-w-[15%] pt-1 px-2 fixed right-14">
 				<!-- section for random common articles -->
+				<h1 class="text-4xl font-bold mb-4 ml-3">Top Picks</h1>
+				<BlogHighlight
+					v-for="a in 5"
+					:key="a" />
 			</aside>
 		</main>
-	</template>
+	</div>
 </template>
 
 <script setup lang="ts"></script>
